eeff153c2f3830bf332e1580e09483440e441b09
AlessandroScrem <48774358+AlessandroScrem@users.noreply.github.com>
2022-01-06 15:32:21 +0100

    add ubo alignas()

--- lib/vk/VulkanVertexBuffer.hpp
+++ lib/vk/VulkanVertexBuffer.hpp
@@ -8,10 +8,19 @@
 // std
 #include <array>
 
+/* 
+    Vulkan expects the data in your structure to be aligned in memory in a specific way, for example:
+
+    Scalars have to be aligned by N (= 4 bytes given 32 bit floats).
+    A vec2 must be aligned by 2N (= 8 bytes)
+    A vec3 or vec4 must be aligned by 4N (= 16 bytes)
+    A nested structure must be aligned by the base alignment of its members rounded up to a multiple of 16.
+    A mat4 matrix must have the same alignment as a vec4.
+ */
 struct UniformBufferObject {
-    glm::mat4 model;
-    glm::mat4 view;
-    glm::mat4 proj;
+    alignas(16) glm::mat4 model;
+    alignas(16) glm::mat4 view;
+    alignas(16) glm::mat4 proj;
 };
 
 
