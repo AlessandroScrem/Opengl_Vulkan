2323a5efe9cbbdcdc6e6f0ca75d53ba46213f0fe
AlessandroScrem <48774358+AlessandroScrem@users.noreply.github.com>
2022-02-26 21:42:48 +0100

    Add Camera movements to Vulkan #4

--- lib/vk/VulkanEngine.cpp
+++ lib/vk/VulkanEngine.cpp
@@ -129,18 +129,13 @@ void VulkanEngine::drawFrame()
 
 void VulkanEngine::updateUbo(uint32_t currentImage)
 {
-    static auto startTime = std::chrono::high_resolution_clock::now();
-
-    auto currentTime = std::chrono::high_resolution_clock::now();
-    float time = std::chrono::duration<float, std::chrono::seconds::period>(currentTime - startTime).count();
- 
-    ubo.model = glm::rotate(glm::mat4(1.0f), time * glm::radians(90.0f), glm::vec3(0.0f, 0.0f, 1.0f));
-    ubo.view = glm::lookAt(glm::vec3(2.0f, 2.0f, 2.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 0.0f, 1.0f));
-
-    auto extent = swapchain.getExtent();
-    ubo.proj = glm::perspective(glm::radians(45.0f), extent.width / (float) extent.height, 0.1f, 10.0f);
+    // rotate model to y up
+    ubo.model = glm::rotate(glm::mat4(1.0f), glm::radians(-90.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+    ubo.view = ourCamera.GetViewMatrix();
+    ubo.proj = glm::perspective(glm::radians(ourCamera.GetFov()), window.getWindowAspect(), 0.1f, 10.0f);
     ubo.proj[1][1] *= -1;
-
+    
+    // update uniform buffer data
     ubo.bind(currentImage);
 
 }
